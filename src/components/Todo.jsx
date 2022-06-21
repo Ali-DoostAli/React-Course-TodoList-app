cf64cbf4e7678a7f868f49a5eaa028fe4bfeb66a
alidoostali <alidoosti630@gmail.com>
2022-06-21 20:02:15 +0430

    remove todo

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,16 +1,15 @@
-const Todo = ({todo, onComplete}) => {
-    return ( 
-        <div
-        className="todo"
-         key={todo.id}>
-            <div className={todo.isCompleted ? "completed" : ""}>{todo.text}</div>
-            <div>
-              <button>Edit</button>
-              <button
-              onClick={onComplete}>complete</button>
-            </div>
-          </div>
-     );
-}
- 
+const Todo = ({ todo, onComplete ,onDelete }) => {
+  return (
+    <div className="todo" key={todo.id}>
+      <div onClick={onComplete} className={todo.isCompleted ? "completed" : ""}>
+        {todo.text}
+      </div>
+      <div>
+        <button>Edit</button>
+        <button onClick={onDelete}>Delete</button>
+      </div>
+    </div>
+  );
+};
+
 export default Todo;
